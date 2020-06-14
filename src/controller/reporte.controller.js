ab55f142146f2021609a56ea6209abc7312aa131
Carlos Bello <bellocarlos2810@gmail.com>
2020-06-13 22:45:37 -0500

    imprimir reporte

--- src/controller/reporte.controller.js
+++ src/controller/reporte.controller.js
@@ -39,6 +39,14 @@ var app = new Vue({
             get()
             .then(res => {
                 this.reporte = res
+                let printContents = document.getElementById('reporte').innerHTML;
+                let originalContents = document.body.innerHTML;
+
+                document.body.innerHTML = printContents;
+
+                window.print();
+
+                document.body.innerHTML = originalContents;
             })
             console.log(this.reporte);
             
